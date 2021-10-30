c1195cb897c29a87aaf2079fdae39ddb1cec0dc3
rodrigortigosa <rodrigortigosa@gmail.com>
2021-10-30 15:06:05 -0300

    Update link para navegacion en Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,9 @@ import { Heroes } from "../components/Heroes";
 import { Container } from "react-bootstrap";
 import { useContext, useEffect } from "react";
 import TeamContext from "../contexts/TeamContext";
-
+import { Row, Col, Button } from "react-bootstrap";
+import { SEARCH } from "../config/router/paths";
+import { Link } from "react-router-dom";
 export function Home() {
   const { team } = useContext(TeamContext);
   useEffect(() => {}, [team]);
@@ -13,7 +15,20 @@ export function Home() {
     <Container className="w-auto">
       <Header />
       <Title text="Equipo" />
-      {team.length === 0 ? <div>Buscar heroes</div> : <Heroes />}
+      {team.length === 0 ? (
+        <Row className="rows-cols-1 justify-content-center my-5 py-3">
+          <Col className="text-center mb-5" xs={8}>
+            <span>El equipo está vacío</span>
+          </Col>
+          <Col className="text-center mb-3" xs={8}>
+            <Link to={SEARCH}>
+              <Button>Buscar heroes</Button>
+            </Link>
+          </Col>
+        </Row>
+      ) : (
+        <Heroes />
+      )}
     </Container>
   );
 }
