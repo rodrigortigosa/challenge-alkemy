3b9c2c5de95c06fd759929148b4910df533920bf
rodrigortigosa <rodrigortigosa@gmail.com>
2021-10-30 14:12:27 -0300

    Fix ordenamiento de powerstats

--- src/components/Heroes.js
+++ src/components/Heroes.js
@@ -6,32 +6,41 @@ import TeamContext from "../contexts/TeamContext";
 export function Heroes() {
   const { team } = useContext(TeamContext);
   const heroes = team;
-  const totalPowerstats = [];
-
-  let totalIntelligence = 0;
-  let totalStrength = 0;
-  let totalSpeed = 0;
-  let totalDurability = 0;
-  let totalPower = 0;
-  let totalCombat = 0;
+  const totalPowerstats = [
+    {
+      powerstat: "Inteligencia",
+      total: 0,
+    },
+    {
+      powerstat: "Fuerza",
+      total: 0,
+    },
+    {
+      powerstat: "Velocidad",
+      total: 0,
+    },
+    {
+      powerstat: "Durabilidad",
+      total: 0,
+    },
+    {
+      powerstat: "Poder",
+      total: 0,
+    },
+    {
+      powerstat: "Combate",
+      total: 0,
+    },
+  ];
 
   for (let i = 0; i < heroes.length; i++) {
-    totalIntelligence += parseInt(heroes[i].powerstats.intelligence);
-    totalStrength += parseInt(heroes[i].powerstats.strength);
-    totalSpeed += parseInt(heroes[i].powerstats.speed);
-    totalDurability += parseInt(heroes[i].powerstats.durability);
-    totalPower += parseInt(heroes[i].powerstats.power);
-    totalCombat += parseInt(heroes[i].powerstats.combat);
+    totalPowerstats[0].total += parseInt(heroes[i].powerstats.intelligence);
+    totalPowerstats[1].total += parseInt(heroes[i].powerstats.strength);
+    totalPowerstats[2].total += parseInt(heroes[i].powerstats.speed);
+    totalPowerstats[3].total += parseInt(heroes[i].powerstats.durability);
+    totalPowerstats[4].total += parseInt(heroes[i].powerstats.power);
+    totalPowerstats[5].total += parseInt(heroes[i].powerstats.combat);
   }
-
-  totalPowerstats.push(totalIntelligence);
-  totalPowerstats.push(totalStrength);
-  totalPowerstats.push(totalSpeed);
-  totalPowerstats.push(totalDurability);
-  totalPowerstats.push(totalPower);
-  totalPowerstats.push(totalCombat);
-
-  /* totalPowerstats.sort((a, b) => a - b).reverse(); */
 
   return (
     <Container className="p-0 w-auto">
@@ -50,43 +59,29 @@ export function Heroes() {
 }
 
 function TotalPowerstats(props) {
-  const totals = [];
-  const totalsNames = [
-    "Inteligencia",
-    "Fuerza",
-    "Velocidad",
-    "Durabilidad",
-    "Poder",
-    "Combate",
-  ];
+  const totals = props.totals;
 
-  for (let i = 0; i < props.totals.length; i++) {
-    let totalsObject = {
-      name: totalsNames[i],
-      number: parseInt(props.totals[i]),
-    };
-    totals.push(totalsObject);
-  }
+  const totalsToShow = []
+    .concat(totals)
+    .sort((a, b) => a.total - b.total)
+    .reverse()
+    .map((item) => {
+      return (
+        <Row
+          className="justify-content-between mx-4 mt-3 mb-3 px-4"
+          key={item.powerstat}
+        >
+          <Col className="text-start">
+            <h5 className="m-0">{item.powerstat}</h5>
+          </Col>
+          <Col className="d-flex justify-content-end align-items-center">
+            <h5 className="m-0">
+              <Badge bg="primary">{item.total}</Badge>
+            </h5>
+          </Col>
+        </Row>
+      );
+    });
 
-  return (
-    <>
-      {totals.map((total) => {
-        return (
-          <Row
-            className="justify-content-between mx-4 mt-3 mb-3 px-4"
-            key={total.name}
-          >
-            <Col className="text-start">
-              <h5 className="m-0">{total.name}</h5>
-            </Col>
-            <Col className="d-flex justify-content-end align-items-center">
-              <h5 className="m-0">
-                <Badge bg="primary">{total.number}</Badge>
-              </h5>
-            </Col>
-          </Row>
-        );
-      })}
-    </>
-  );
+  return totalsToShow;
 }
